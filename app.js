c5611812566d1ff81e4fc3d8889aafd3309d39b1
SangYoonLee <prestro1231@hufs.ac.kr>
2024-03-26 03:09:14 +0900

    Modify: vdom을 보다 복잡한 예제로 변경

--- app.js
+++ app.js
@@ -19,6 +19,27 @@ const vdom = {
       props: {},
       children: ["React 만들기"],
     },
+    {
+      tag: "ul",
+      props: {},
+      children: [
+        {
+          tag: "li",
+          props: {},
+          children: ["첫 번째 아이템"],
+        },
+        {
+          tag: "li",
+          props: {},
+          children: ["두 번째 아이템"],
+        },
+        {
+          tag: "li",
+          props: {},
+          children: ["세 번째 아이템"],
+        },
+      ],
+    },
   ],
 };
 
